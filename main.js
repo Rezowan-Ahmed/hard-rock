a956bd4dd107d23e7a51a17aa54f433f983fb4a5
Rezowan-Ahmed <ahmedrezowan07@gmail.com>
2020-08-17 17:14:56 +0800

    assignment done

--- main.js
+++ main.js
@@ -1,37 +1,36 @@
-const form = document.getElementById("form");
-const search = document.getElementById("search");
-const result = document.getElementById("result");
-const apiURL = 'https://api.lyrics.ovh';
-
-form.addEventListener('submit', e=>{
+const urlOfApi = 'https://api.lyrics.ovh';
+const songNameInput = document.getElementById("songNameInput");
+const formForInputSearch = document.getElementById("formForInputSearch");
+formForInputSearch.addEventListener('submit', e=>{
     e.preventDefault();
-    searchValue = search.value.trim();
+    searchValue = songNameInput.value.trim();
     
     if (!searchValue) {
-        alert('There is nothing');
+        alert('Song not available. please try later');
     }
     else{
-        searchSong(searchValue);
+        songSearch(searchValue);
     }
 })
 
- async function searchSong(searchValue) {
-    const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`)
+ async function songSearch(searchValue) {
+    const searchResult = await fetch(`${urlOfApi}/suggest/${searchValue}`)
     const data = await searchResult.json();
 
-    showData(data);
+    resultShow(data);
 
 }
 
-function showData(data) {
+function resultShow(data) {
     console.log(data);
-    result.innerHTML = `
+    fancyResults.innerHTML = `
     <ul class="song-list">
     ${data.data.slice(0,10).map(song => `<li>
                         <div class="single-result row align-items-center my-3 p-3">
                     <div class="col-md-9">
                         <h3 class="lyrics-name">${song.title}</h3>
-                        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                        <p class="author lead">Album: <span>${song.artist.name}</span></p>
+                        <p class="author lead">Artist: <span>${song.album.title}</span></p>
                     </div>
                     <div class="col-md-3 text-md-right text-center">
                         <button class="btn btn-success"> <span data-artist="${song.artist.name}" data-songTitle="${song.title}"> Get Lyrics </span> </button>
@@ -43,7 +42,8 @@ function showData(data) {
     `
 }
 
-result.addEventListener("click", e=>{
+const fancyResults = document.getElementById("fancyResults");
+fancyResults.addEventListener("click", e=>{
     const clickedElement = e.target;
     if (clickedElement.tagName === "span" || "button") {
         const artist = clickedElement.getAttribute('data-artist');
@@ -54,14 +54,14 @@ result.addEventListener("click", e=>{
 })
 
 async function getLyrics(artist, songTitle) {
-    const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const response = await fetch(`${urlOfApi}/v1/${artist}/${songTitle}`);
     const data = await response.json(); 
     const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
     console.log(lyrics);
 
-     result.innerHTML =    ` <div class="single-lyrics text-center">
-                                 <h2 class="text-success mb-4"><strong>${songTitle}</strong><br>- ${artist}</h2>
-                                 <pre class="lyric text-white">${lyrics}</pre>
-                             </div>
-                             `
+    fancyResults.innerHTML =    
+     ` <div class="single-lyrics text-center">
+    <h2 class="text-success mb-4"><bold>${songTitle}</bold><br> by ${artist}</h2>
+    <pre class="lyric text-white">${lyrics}</pre>
+    </div>`
 }
